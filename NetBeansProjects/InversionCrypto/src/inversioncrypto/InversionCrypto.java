0fa15bab775aa4f6487c3bcc1d4dcd53308ee737
renzo <69172164+renzo-carrasco@users.noreply.github.com>
2021-03-08 13:59:27 -0300

    Add files via upload

---
+++ NetBeansProjects/InversionCrypto/src/inversioncrypto/InversionCrypto.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inversioncrypto;
+
+/**
+ *
+ * @author RenzoCarrasco
+ */
+public class InversionCrypto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("MUNDO CRYPTOGRAFICO");
+        
+        /* COMENTARIO DE PARRAFO
+            COMENTARIO DE BARRIOS PARRAFOS
+        */
+        
+        // COMENTARIO DE LINEA
+        
+        /*  Declaracion de variables
+        
+              tipo identificador;
+        */
+        
+        /*  Asignacion ( operador = )
+                x = 10;
+        */
+        
+        /*  Tipo de dato enteros
+        byte: -128 a +127 ( 1 byte )
+        short: -32768 a +32767 ( 2 bytes )
+        int: -2147483648 a +2147483647 ( 4 bytes )
+        long: -9223372036854775808 a +9223372036854775807 ( 8 bytes )           
+        */
+        
+        byte num;
+        
+        num = 235;
+        System.out.println( num );
+    }
+    
+}
